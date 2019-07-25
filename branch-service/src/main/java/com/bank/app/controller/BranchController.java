e912e0352ffb1baadb2fa3bdc2c6c5c2cbeb87aa
Anil Saladi <anil.saladi-ext@ignitionone.com>
2019-07-25 14:03:39 +0530

    commented branchserviceProxy.java

--- branch-service/src/main/java/com/bank/app/controller/BranchController.java
+++ branch-service/src/main/java/com/bank/app/controller/BranchController.java
@@ -1,30 +1,20 @@
 package com.bank.app.controller;
 
-import java.util.Collection;
-import java.util.List;
-
 import com.bank.app.domain.Bank;
-import com.bank.app.repository.BranchServiceProxy;
-import com.bank.app.serviceImpl.BranchServiceImpl;
 import com.bank.app.domain.Branch;
+import com.bank.app.serviceImpl.BranchServiceImpl;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import org.json.JSONException;
 import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import java.util.Collection;
 
 @RestController
 @RequestMapping("/branch")
 @JsonIgnoreProperties
-@RefreshScope
 public class BranchController {
 /*
 	@Autowired
@@ -35,8 +25,8 @@ public class BranchController {
 
     Branch branch = new Branch();
 
-    @Autowired
-    BranchServiceProxy branchServiceProxy;
+    /*@Autowired
+    BranchServiceProxy branchServiceProxy;*/
 
     @Autowired
     private BranchServiceImpl branchServiceImpl;
@@ -44,7 +34,7 @@ public class BranchController {
     @RequestMapping(value = "/getBranchById/{branchCode}", method = RequestMethod.GET, headers = "Accept=application/json")
     @ResponseBody
     public String getBranchById(@PathVariable Integer branchCode) throws JSONException {
-        Collection<Bank> list = branchServiceProxy.findAll();
+        //Collection<Bank> list = branchServiceProxy.findAll();
         String branch = branchServiceImpl.getBranchById(branchCode);
         return branch;
     }
